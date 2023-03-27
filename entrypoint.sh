e8eab2c1207c904a1fc18b1a825c82e3c8c1d71d
Emil Lerch <emil@lerch.org>
2023-03-27 14:25:16 -0700

    add action.yml/sort environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-env
+env | sort
+#GITHUB_RUN_NUMBER
+# Job status: success/failure/cancelled
 
-
+      #   if: success()
+      #   run: |
+      #          curl -u '${{ secrets.NTFY_USER }}:${{ secrets.NTFY_PASSWORD }}' \
+      #          -H 'Title: Job Succeeded. ${{ github.repository }}/${{ github.ref }}' \
+      #          -H "Tags: +1" \
+      #          -d 'Job succeeded. See '$GITHUB_SERVER_URL'/${{ github.repository }}/actions/runs/${{ github.run_number }} for details' \
+      #          '${{ secrets.NTFY_HOST }}${{ secrets.NTFY_TOPIC }}'
+      # - name: Failure Notification
+      #   if: failure()
+      #   run: |
+      #          curl -u '${{ secrets.NTFY_USER }}:${{ secrets.NTFY_PASSWORD }}' \
+      #          -H "Priority: high" \
+      #          -H 'Title: Job FAILED. ${{ github.repository }}/${{ github.ref }}' \
+      #          -H "Tags: warning" \
+      #          -d 'Job failed. See '$GITHUB_SERVER_URL'/${{ github.repository }}/actions/runs/${{ github.run_number }} for details' \
+      #          '${{ secrets.NTFY_HOST }}${{ secrets.NTFY_TOPIC }}'
+      #
