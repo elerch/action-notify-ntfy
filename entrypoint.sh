13f3bdf30afa5943ff900f79a65163afe7ca8ffb
Emil Lerch <emil@lerch.org>
2023-03-27 15:13:38 -0700

    fix auth

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,21 +6,20 @@ echo "Notifying job status: $1"
 # Job status: success/failure/cancelled
 
 if [ "$1" = "success" ]; then
-  curl -u "${INPUT_USER}:${INPUT__PASSWORD}" \
+  curl -s -u "${INPUT_USER}:${INPUT_PASSWORD}" \
     -H "Title: Job Succeeded. ${GITHUB_REPOSITORY}/${GITHUB_REF}" \
     -H "Tags: +1" \
     -d "Job succeeded. See $GITHUB_SERVER_URL/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_NUMBER} for details" \
     "${INPUT_HOST}${INPUT_TOPIC}"
 elif [ "$1" = "failure" ]; then
-  curl -u "${INPUT_USER}:${INPUT__PASSWORD}" \
+  curl -s -u "${INPUT_USER}:${INPUT_PASSWORD}" \
     -H "Title: Job FAILED. ${GITHUB_REPOSITORY}/${GITHUB_REF}" \
     -H "Priority: high" \
     -H "Tags: warning" \
     -d "Job failed. See $GITHUB_SERVER_URL/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_NUMBER} for details" \
     "${INPUT_HOST}${INPUT_TOPIC}"
 elif [ "$1" = "cancelled" ]; then
-  # square or x with circle
-  curl -u "${INPUT_USER}:${INPUT__PASSWORD}" \
+  curl -s -u "${INPUT_USER}:${INPUT_PASSWORD}" \
     -H "Title: Job cancelled. ${GITHUB_REPOSITORY}/${GITHUB_REF}" \
     -H "Priority: low" \
     -H "Tags: x" \
